5c6bc0e4e4f420f14ddaeb4114215e56651bbee5
DouglasDainese <douglas_dainese@hotmail.com>
2022-08-12 01:23:51 -0300

    Requisito 20 finalizado

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const loginButton = document.querySelector('#btn-Entrar');
-const btnenviar = document.getElementById('submit-btn');
-const boxacordo = document.getElementById('agreement');
+const btnEnviar = document.getElementById('submit-btn');
+const boxAcordo = document.getElementById('agreement');
+const totalCaracteres = document.getElementById('textarea');
+const contador = document.getElementById('counter');
+
 function testLogin() {
   const emailInput = document.querySelector('#input-Email');
   const senhaInput = document.querySelector('#input-Senha');
@@ -13,15 +16,22 @@ function testLogin() {
   }
 }
 loginButton.addEventListener('click', testLogin);
-btnenviar.disabled = true;
+btnEnviar.disabled = true;
 
-console.log(boxacordo);
-function habilitarbtn() {
-  if (btnenviar.disabled === true) {
-    btnenviar.disabled = false;
+function habilitarBtn() {
+  if (btnEnviar.disabled === true) {
+    btnEnviar.disabled = false;
   } else {
-    btnenviar.disabled = true;
+    btnEnviar.disabled = true;
   }
 }
+boxAcordo.addEventListener('click', habilitarBtn);
 
-boxacordo.addEventListener('click', habilitarbtn);
+function contarCaracteres() {
+  const caracteres = totalCaracteres.value.length;
+  const caracteresRestante = 500 - caracteres;
+  const resultado = parseInt(caracteresRestante, 10);
+  contador.innerText = resultado;
+}
+contarCaracteres();
+totalCaracteres.addEventListener('keyup', contarCaracteres);
